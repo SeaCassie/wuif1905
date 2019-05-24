d5fbfa10aed08141e93c951b46c5267233028b27
SeaCassie <1143391936@qq.com>
2019-05-24 21:57:21 +0800

    “f2019-5-24 购物车添加上去

--- mobilelayout/Js/tab.js
+++ mobilelayout/Js/tab.js
@@ -1,23 +1,24 @@
 
+// 选项卡的js的设计，注意active,需要在样式表里面去设置active的样式表
+function  tab(a,b,c){
 
-    var oCard= document.getElementById('shopCard');
-    var aLi = oCard.getElementsByTagName("li");
+    var oCard = document.getElementById(a);
+    var aLi = oCard.getElementsByTagName(b);
 
-        // var aDiv=oCard.getElementsByTagName('div');
-    var aDiv = oCard.getElementsByTagName("div");
-
-    // console.log(aLi.length);
-    //     // console.log(aDiv.length);
-    for(var i=0;i<aLi.length;i++)
-    {
-        //alert(i); //0 1 2
+// var aDiv=oCard.getElementsByTagName('div');
+    var aDiv = oCard.getElementsByTagName(c);
+    // console.log("123456")
+    console.log(aLi.length);
+    console.log(aDiv.length);
+    for (var i = 0; i < aLi.length; i++) {
+        // alert(i); //0 1 2
 
         aLi[i].index = i;//循环为每一个标题li元素添加一个index属性并赋值
 
-        aLi[i].onclick = function(){
-
-            for(var j=0;j<aLi.length;j++)
-            {
+        aLi[i].onclick = function () {
+            // console.log("456");
+            for (var j = 0; j < aLi.length; j++) {
+                console.log(j)
                 //将所有的标题li元素全部不激活
                 aLi[j].className = "";
 
@@ -26,6 +27,7 @@
             }
 
             //设置当前的标题li元素激活
+            // console.log(i);
             this.className = "active";
 
             //设置当前的内容div元素激活
@@ -34,3 +36,4 @@
             aDiv[this.index].style.display = "block";
         }
     }
+}
